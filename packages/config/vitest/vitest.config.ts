6ce9cb404b64665ce7a98e083109bba594b2f559
kenwoo9y <kenwoo9y@gmail.com>
2025-09-05 21:21:11 +0900

    add vitest.config.ts

---
+++ packages/config/vitest/vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    exclude: ['node_modules', 'dist'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      thresholds: {
+        statements: 80,
+        branches: 80,
+        functions: 80,
+        lines: 80,
+      },
+    },
+  },
+});
