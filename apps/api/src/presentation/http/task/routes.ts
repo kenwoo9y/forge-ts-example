cc4aa24e34a71b0ad4b117972950e05be5a3e686
kenwoo9y <kenwoo9y@gmail.com>
2026-02-11 14:10:52 +0000

    Modified to use @hono/zod-validator

--- apps/api/src/presentation/http/task/routes.ts
+++ apps/api/src/presentation/http/task/routes.ts
@@ -1,13 +1,15 @@
+import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
+import { createTaskSchema, updateTaskSchema } from 'schemas';
 import { createTaskHandler, type TaskHandlerDeps } from './handler.js';
 
 export function createTaskRoutes(deps: TaskHandlerDeps) {
   const app = new Hono();
   const handler = createTaskHandler(deps);
 
-  app.post('/tasks', handler.createTask);
+  app.post('/tasks', zValidator('json', createTaskSchema), handler.createTask);
   app.get('/tasks/:id', handler.getTask);
-  app.patch('/tasks/:id', handler.updateTask);
+  app.patch('/tasks/:id', zValidator('json', updateTaskSchema), handler.updateTask);
   app.delete('/tasks/:id', handler.deleteTask);
 
   return app;
--- apps/api/src/presentation/http/user/routes.ts
+++ apps/api/src/presentation/http/user/routes.ts
@@ -1,13 +1,15 @@
+import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
+import { createUserSchema, updateUserSchema } from 'schemas';
 import { createUserHandler, type UserHandlerDeps } from './handler.js';
 
 export function createUserRoutes(deps: UserHandlerDeps) {
   const app = new Hono();
   const handler = createUserHandler(deps);
 
-  app.post('/users', handler.createUser);
+  app.post('/users', zValidator('json', createUserSchema), handler.createUser);
   app.get('/users/:username', handler.getUser);
-  app.patch('/users/:username', handler.updateUser);
+  app.patch('/users/:username', zValidator('json', updateUserSchema), handler.updateUser);
   app.delete('/users/:username', handler.deleteUser);
   app.get('/users/:username/tasks', handler.getUserTasks);
 
