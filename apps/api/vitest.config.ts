a00a5775c8f70776408bbfd5bb768493a6c79bc3
kenwoo9y <kenwoo9y@gmail.com>
2026-02-14 15:51:31 +0000

    Implement vitest configuration

---
+++ apps/api/vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, mergeConfig } from 'vitest/config';
+import baseConfig from '../../packages/config/vitest/vitest.config.ts';
+
+export default mergeConfig(
+  baseConfig,
+  defineConfig({
+    test: {
+      root: '.',
+      exclude: ['node_modules', 'dist', 'src/infrastructure/**'],
+      coverage: {
+        include: ['src/**'],
+        exclude: [
+          'src/infrastructure/**',
+          'src/index.ts',
+          'src/**/dto.ts',
+          'src/**/repository.ts',
+          'src/**/queryService.ts',
+        ],
+      },
+    },
+  })
+);
